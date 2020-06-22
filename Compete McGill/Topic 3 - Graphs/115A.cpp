df8b572f9376d601f665bd09e0839e14528260ae
Hung <Le.nhthung@gmail.com>
2020-06-22 19:31:41 -0300

    Compete McGill topic 3 in progress

---
+++ Compete McGill/Topic 3 - Graphs/115A.cpp
@@ -0,0 +1,29 @@
+// Link: https://codeforces.com/problemset/problem/115/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    /**
+     * Find longest path length
+     */
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, i, j, len, longest=1;
+    cin >> n;
+    int g[n+1];
+
+    for(i=1; i<=n; ++i) cin >> g[i];
+    for(i=1; i<=n; ++i){
+        len=1;
+        j=i;
+        while(g[j]!=-1){
+            j=g[j];
+            len++;
+        }
+        longest=max(longest, len);
+    }
+    cout << longest << "\n";
+}
